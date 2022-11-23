4ca6f7f717a529f26d49e33c6726452413a76f1c
Natalie <fogel666@yandex.ru>
2022-11-23 15:24:05 +0300

    button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h3">Text</Htag>
+      <Button appearance='primary' arrow="right">Еу</Button>
+      <Button appearance='ghost' arrow="down">цуацйу</Button>
     </div>
   );
 }
