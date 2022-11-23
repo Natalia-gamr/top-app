33fe430b12bc7ca839e0e4d57f728b97080ebb1f
Natalie <fogel666@yandex.ru>
2022-11-23 16:22:36 +0300

    p

---
+++ components/P/P.tsx
@@ -0,0 +1,19 @@
+import { PProps } from "./P.props";
+import styles from './P.module.css';
+import cn from 'classnames';
+
+
+export const P = ({ children, size = "M", className, ...props }: PProps): JSX.Element => {
+    return (
+        <p className={cn(styles.p, className, {
+            [styles.S]: size == 'S',
+            [styles.L]: size == 'L',
+            [styles.M]: size == 'M'
+        })}
+            {...props}>
+            {children}
+
+        </p>
+
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Htag } from "../components";
+import { Button, Htag, P } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -6,6 +6,9 @@ export default function Home(): JSX.Element {
       <Htag tag="h3">Text</Htag>
       <Button appearance='primary' arrow="right">Еу</Button>
       <Button appearance='ghost' arrow="down">цуацйу</Button>
+      <P size="L">sdgaserg</P>
+      <P size="S">argre</P>
+      <P>argaerw</P>
     </div>
   );
 }
