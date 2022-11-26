5ec9e39cff6a84ed759f3b79bfeab2005d60747c
Natalie <fogel666@yandex.ru>
2022-11-26 15:04:58 +0300

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
+import { Layout, withLayout } from "../layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4)
 
   return (
@@ -19,3 +20,5 @@ export default function Home(): JSX.Element {
     </>
   );
 }
+
+export default withLayout(Home)
